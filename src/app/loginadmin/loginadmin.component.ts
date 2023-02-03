29373b38fff3dc296c910723e2d1b29e9bb72107
lauraparentela <laura.parentela@studio.unibo.it>
2023-02-03 15:17:17 +0100

    15:15

---
+++ src/app/loginadmin/loginadmin.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import{FormGroup, FormBuilder} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-login',
+  templateUrl: './loginadmin.component.html',
+  styleUrls: ['./loginadmin.component.css']
+})
+export class LoginadminComponent implements OnInit{
+  public loginForm !: FormGroup
+  constructor(private formBuilder : FormBuilder, private http:HttpClient, private router:Router) { }
+  ngOnInit():void {
+    this.loginForm=this.formBuilder.group({
+      nomeutente:[''],
+      password:['']
+    })
+  }
+  login(){
+    this.http.get<any>("http://localhost:3000/amministratore")
+      .subscribe(res=> {
+        const user = res.find((a: any) => {
+          return a.nomeutente === this.loginForm.value.nomeutente && a.password === this.loginForm.value.password
+        });
+        if (user) {
+          alert("Login avvenuto con successo");
+          this.loginForm.reset();
+          this.router.navigate(['homepageutente'])
+        } else {
+          alert("Nome utente non trovato");
+        }
+      }, err =>{
+        alert("Qualcosa è andato storto")
+      })
+  }
+}
